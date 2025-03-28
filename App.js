db72659d74e4fb41798c92c8f0d5bd23306d8062
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-28 11:37:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tab one </Text>
+      <Text>Tab one is here ! </Text>
       <StatusBar style="auto" />
     </View>
   );
