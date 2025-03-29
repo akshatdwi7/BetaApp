ad08c6b706be22423a8aa4164642ca347952eaff
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-29 10:48:32 +0530

    testing

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tab one is here !... </Text>
+      <Text>Tab one is here !... </Text>
       <StatusBar style="auto" />
     </View>
   );
