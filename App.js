30fc9d4fcae807775d761885bc171972ca6f7848
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-03-30 22:51:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={{ color: "red", margin: 10 }}>Tab one is here !... </Text>
       <Text>Tab one is here !... </Text>
-      <Text>Tab one is here !... </Text>
+      <Button title="tap me" />
 
       <StatusBar style="auto" />
     </View>
