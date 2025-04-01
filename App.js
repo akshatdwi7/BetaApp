54a686b34d737840ad69c13f5a5ee8a8bcfd1b5d
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-04-01 13:50:21 +0530

    added function for B

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+  }
+  function addGoalHandler() {}
+
   return (
     <View style={styles.appcontainer}>
-      <View>
-        <TextInput placeholder="Your  course Goals! " />
+      <View style={styles.inputcontainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your  course Goals! "
+          onChangeText={goalInputHandler} // this is the react native way to handle text input
+        />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -18,5 +27,27 @@ export default function App() {
 const styles = StyleSheet.create({
   appcontainer: {
     padding: 50,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  inputcontainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    borderRadius: 6,
+    padding: 10,
+    marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
